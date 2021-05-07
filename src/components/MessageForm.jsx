fe12dd5411b49dbb6058f84c95db6a38f39b396a
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-05-07 20:51:42 +0530

    feat - message-form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,69 @@
-const MessageForm = () => {
-	return <div className="">MessageForm</div>;
+import React, { useState } from "react";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
+import { sendMessage, isTyping } from "react-chat-engine";
+
+const MessageForm = (props) => {
+	//& destructuring
+	const { creds, chatId } = props;
+	const authObject = {
+		"Project-ID": props.projectID,
+		"User-Name": props.userName,
+		"User-Secret": props.userSecret,
+	};
+
+	//& states
+	const [value, setValue] = useState("");
+
+	const onSubmitHandler = (e) => {
+		e.preventDefault();
+
+		const text = value.trim();
+
+		if (text.length > 0);
+		sendMessage(creds, chatId, { text: text });
+
+		setValue("");
+	};
+
+	//TODO isTyping
+	const onChangeHandler = (e) => {
+		setValue(e.target.value);
+		//isTyping(authObject, chatId);
+	};
+
+	const onUploadHandler = (e) => {
+		sendMessage(creds, chatId, { files: e.target.files, text: "" });
+	};
+
+	return (
+		<form className="message-form" onSubmit={onSubmitHandler}>
+			<input
+				className="message-input"
+				placeholder="enter your message..."
+				value={value}
+				onChange={onChangeHandler}
+				onSubmit={onSubmitHandler}
+			/>
+
+			<label htmlFor="upload-button">
+				<span className="image-button">
+					<PictureOutlined className="picture-icon" />
+				</span>
+			</label>
+
+			<input
+				type="file"
+				id="upload-button"
+				multiple={false}
+				style={{ display: "none" }}
+				onChange={onUploadHandler}
+			/>
+
+			<button type="submit" className="send-button">
+				<SendOutlined className="send-icon" />
+			</button>
+		</form>
+	);
 };
 
 export default MessageForm;
